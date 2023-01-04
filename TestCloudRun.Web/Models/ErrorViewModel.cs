ae7d9d7597ddf2a875c968ca9632eb031342f655
dpaez1 <dpaez@disersoft.net>
2023-01-04 15:18:34 -0500

    Se agrega proyecto Web para pruebas

---
+++ TestCloudRun.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestCloudRun.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
